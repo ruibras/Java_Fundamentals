5bff0f4deed01cfe2ac5791052aebf89acfd4d89
ruibras <ruixmxbras@gmail.com>
2014-05-10 12:58:28 +0100

    aula 2

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,9 +1,15 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World v2");
-
+		//System.out.println("Hello World v2");
+//using ternary operator
+		for (int i = 0; i < args.length - 1; i++) {
+			System.out.print(args[i]);
+			
+			//System.out.print(args[i] + ((i == args.length - 1) ? "\n" : " "));
+			
+		}
 	}
-
 }
+
+
