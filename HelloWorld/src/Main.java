a2b2b5630c6506026f91f579be7e8346741bddb9
ruibras <ruixmxbras@gmail.com>
2014-05-17 13:01:00 +0100

    first commit

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,12 +3,25 @@ public class Main {
 	public static void main(String[] args) {
 		//System.out.println("Hello World v2");
 //using ternary operator
-		for (int i = 0; i < args.length - 1; i++) {
+	/*	for (int i = 0; i < args.length - 1; i++) {
 			System.out.print(args[i]);
 			
 			System.out.print(args[i] + ((i == args.length - 1) ? "\n" : " "));
 			
-		}
+		}*/
+		
+		int x =1/5;
+		System.out.println(x);
+		
+		
+		double y =1/30;
+				System.out.println(y);
+				
+		char z =2;
+		System.out.println(z);
+
+
+		
 	}
 }
 
